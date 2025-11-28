e8bca7dd3c92f58957a7ffed83d52e918d214f90
shital552 <liujim928@gmail.com>
2025-11-28 10:48:47 +0800

    PY

---
+++ HOME/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lalaport",
+      "name": "lalaport門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5454625342318735,
+        "pitch": -0.31234736989108924,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.109348140252534,
+          "pitch": -0.5269230382377916,
+          "title": "LALAPORT",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "HOME",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
